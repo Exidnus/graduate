1dbe78a943cbfd807e4f3d079733a6f27f31f205
Dmitriy Varygin <dmitriy.varygin@yandex.ru>
2016-04-05 22:00:05 +0500

    Working on web layer

--- src/main/java/ru/dvvar/graduate/web/RestaurantController.java
+++ src/main/java/ru/dvvar/graduate/web/RestaurantController.java
@@ -1,7 +1,44 @@
 package ru.dvvar.graduate.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.dvvar.graduate.model.Restaurant;
+import ru.dvvar.graduate.service.RestaurantService;
+
+import java.util.List;
+
 /**
  * Created by Dmitriy_Varygin on 05.04.2016.
  */
 public class RestaurantController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RestaurantController.class);
+
+    private RestaurantService service;
+
+    public Restaurant create(Restaurant restaurant) {
+        restaurant.setId(null);
+        LOG.info("create " + restaurant);
+        return service.save(restaurant);
+    }
+
+    public Restaurant get(int id) {
+        LOG.info("get " + id);
+        return service.get(id);
+    }
+
+    public void delete(int id) {
+        LOG.info("delete " + id);
+        service.delete(id);
+    }
+
+    public void update(Restaurant restaurant) {
+        LOG.info("update " + restaurant);
+        service.update(restaurant);
+    }
+
+    public List<Restaurant> getAll() {
+        LOG.info("getAll");
+        return service.getAll();
+    }
 }
--- src/main/java/ru/dvvar/graduate/web/VoteController.java
+++ src/main/java/ru/dvvar/graduate/web/VoteController.java
@@ -1,7 +1,21 @@
 package ru.dvvar.graduate.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.dvvar.graduate.model.Vote;
+
 /**
  * Created by Dmitriy_Varygin on 05.04.2016.
  */
 public class VoteController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(VoteController.class);
+
+    public void vote(Vote vote, int userId) {
+        LOG.info("vote {} user with id {}", vote, userId);
+    }
+
+    public void cancel(Vote vote, int userId) {
+        LOG.info("cancel {} user with id {}", vote, userId);
+    }
 }
