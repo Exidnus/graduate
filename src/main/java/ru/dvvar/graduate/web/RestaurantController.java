a196eaac55c3c2c3204647cece890dad618b555e
Dmitriy Varygin <dmitriy.varygin@yandex.ru>
2016-04-05 21:32:43 +0500

    Working on web layer

---
+++ src/main/java/ru/dvvar/graduate/web/RestaurantController.java
@@ -0,0 +1,7 @@
+package ru.dvvar.graduate.web;
+
+/**
+ * Created by Dmitriy_Varygin on 05.04.2016.
+ */
+public class RestaurantController {
+}
---
+++ src/main/java/ru/dvvar/graduate/web/UserController.java
@@ -0,0 +1,44 @@
+package ru.dvvar.graduate.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.dvvar.graduate.model.User;
+import ru.dvvar.graduate.service.UserService;
+
+import java.util.List;
+
+/**
+ * Created by Dmitriy_Varygin on 05.04.2016.
+ */
+public class UserController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
+
+    UserService service;
+
+    public User create(User user) {
+        user.setId(null);
+        LOG.info("create " + user);
+        return service.save(user);
+    }
+
+    public User get(int id) {
+        LOG.info("get " + id);
+        return service.get(id);
+    }
+
+    public void delete(int id) {
+        LOG.info("delete " + id);
+        service.delete(id);
+    }
+
+    public void update(User user) {
+        LOG.info("update " + user);
+        service.update(user);
+    }
+
+    public List<User> getAll() {
+        LOG.info("getAll ");
+        return service.getAll();
+    }
+}
